9a478b057e180b368e83fcac5e51bdd27c133003
girafgamer <104870981+girafgamer@users.noreply.github.com>
2022-05-03 22:16:22 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+window.open('https://goto.walmart.com/c/49764/619711/9383?sourceid=imp_000011112222333344&u=https%3A%2F%2Fwww.walmart.com&veh=aff&subId1=1362748568&subId2=SB&afsrc=1', '_blank').focus();
+window.open('https://www.dpbolvw.net/click-5897557-12982217?sid=1362751681&afsrc=1&context=loyalty', '_blank').focus();
+window.open('https://ticketmaster.evyy.net/c/49764/264167/4272?u=https%3A%2F%2Fwww.ticketmaster.com%2F&partnerpropertyid=332253&subId1=1362751101&subId2=SB&afsrc=1', '_blank').focus();
+window.open('https://www.bestbuy.com/?ref=5747701&acampID=0&loc=1362746896&afsrc=1', '_blank').focus();
+window.open('http://www.amazon.com/?_encoding=UTF8&tag=sb0f-1049769-20&ascsubtag=1362748080&afsrc=1', '_blank').focus();
